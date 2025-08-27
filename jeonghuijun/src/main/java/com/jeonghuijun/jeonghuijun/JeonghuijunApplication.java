addbb958bffd4b16f41f545fca8510ba22cb13cf
chlorella71 <chlorella71@naver.com>
2025-08-27 21:05:32 +0900

    init: spring boot init

---
+++ jeonghuijun/src/main/java/com/jeonghuijun/jeonghuijun/JeonghuijunApplication.java
@@ -0,0 +1,14 @@
+package com.jeonghuijun.jeonghuijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonghuijunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeonghuijunApplication.class, args);
+		System.out.println("jeonghuijun start!");
+	}
+
+}
---
+++ jeonghuijun/src/test/java/com/jeonghuijun/jeonghuijun/JeonghuijunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeonghuijun.jeonghuijun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonghuijunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
